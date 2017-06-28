a83ff80fed7d45a977fe2e0b1c3663b619d40235
chenjingfeng <lol@LAPTOP-4GBVK83H>
2017-06-28 09:26:07 +0800

    cjf

---
+++ src/main/java/cn/springboot/SpringBootFrist/TestGit.java
@@ -0,0 +1,14 @@
+package cn.springboot.SpringBootFrist;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGit {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
