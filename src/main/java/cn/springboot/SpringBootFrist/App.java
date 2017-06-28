589d5b8773b195db2d184f1cb73a7682e30f30df
PDC <PDC@DESKTOP-0IAKAJ6>
2017-06-28 16:46:42 +0800

    第一天修改

--- src/main/java/cn/springboot/SpringBootFrist/App.java
+++ src/main/java/cn/springboot/SpringBootFrist/App.java
@@ -1,13 +1,25 @@
 package cn.springboot.SpringBootFrist;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
+@Controller
+@EnableAutoConfiguration
+public class App {
+	@RequestMapping("/")
+	@ResponseBody
+	String home(){
+		return "hello world";
+	}
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
     }
 }
